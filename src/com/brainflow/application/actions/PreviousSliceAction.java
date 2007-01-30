6f157d586a13ec917d744df337a5e9a7d3c55851
brad.buchsbaum <brad.buchsbaum@922ecd08-ad19-0410-8a94-01093a00bd73>
2007-01-30 04:02:07 +0000

    Failed commit: Default (2)

---
+++ src/com/brainflow/application/actions/PreviousSliceAction.java
@@ -0,0 +1,44 @@
+package com.brainflow.application.actions;
+
+import org.bushe.swing.action.BasicAction;
+
+import java.awt.event.ActionEvent;
+
+import com.brainflow.core.ImageView;
+import com.brainflow.display.Crosshair;
+import com.brainflow.image.anatomy.AnatomicalVolume;
+import com.brainflow.image.space.Axis;
+import com.brainflow.image.axis.ImageAxis;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Brad Buchsbaum
+ * Date: Jan 29, 2007
+ * Time: 12:36:45 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PreviousSliceAction extends BasicAction {
+
+
+    protected void execute(ActionEvent evt) throws Exception {
+
+        ImageView view = (ImageView) getContextValue(ActionContext.SELECTED_IMAGE_VIEW);
+
+        if (view != null) {
+           Crosshair cross = view.getCrosshair();
+           AnatomicalVolume displayAnatomy = view.getDisplayAnatomy();
+           Axis axis = cross.getViewport().getBounds().findAxis(displayAnatomy.ZAXIS);
+           ImageAxis iaxis = view.getImageDisplayModel().getImageAxis(axis);
+
+           int sample = iaxis.nearestSample(cross.getValue(displayAnatomy.ZAXIS));
+           int nsample = sample -1;
+           if (nsample >= 0 && nsample <iaxis.getNumSamples()) {
+               cross.setValue(iaxis.valueOf(nsample));
+           }
+        }
+
+
+    }
+
+
+}

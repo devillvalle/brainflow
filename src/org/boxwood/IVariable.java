736a6bdbafecfd0ef41e711fdb44d708029f8647
brad.buchsbaum <brad.buchsbaum@922ecd08-ad19-0410-8a94-01093a00bd73>
2008-03-04 05:45:05 +0000

    Failed commit: Default (2)

---
+++ src/org/boxwood/IVariable.java
@@ -0,0 +1,19 @@
+package org.boxwood;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Brad Buchsbaum
+ * Date: Mar 3, 2008
+ * Time: 9:37:54 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface IVariable<T> {
+
+    public T valueAt(int i);
+
+    public int length();
+
+
+
+
+}

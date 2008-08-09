f0ebd91de789ea6d73069273c01cf921e649a7f7
brad.buchsbaum <brad.buchsbaum@922ecd08-ad19-0410-8a94-01093a00bd73>
2008-08-09 20:14:18 +0000

    Added number labels to BiSlider

---
+++ trunk/src/com/brainflow/application/presentation/binding/ToggleBarAdapterX.java
@@ -0,0 +1,56 @@
+package com.brainflow.application.presentation.binding;
+
+import com.jidesoft.swing.CheckBoxList;
+import com.brainflow.gui.ToggleBar;
+
+import java.util.List;
+
+import net.java.dev.properties.binding.swing.adapters.SwingAdapter;
+import net.java.dev.properties.BaseProperty;
+
+import javax.swing.event.ListSelectionListener;
+import javax.swing.event.ListSelectionEvent;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Brad
+ * Date: Jul 31, 2008
+ * Time: 10:41:05 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ToggleBarAdapterX extends SwingAdapter<Integer, ToggleBar> implements ListSelectionListener {
+    protected void bindListener(BaseProperty<Integer> integerBaseProperty, ToggleBar component) {
+        component.addListSelectionListener(this);
+    }
+
+    protected void unbindListener(BaseProperty<Integer> integerBaseProperty, ToggleBar component) {
+        component.removeListSelectionListener(this);
+
+    }
+
+    private void joke() {}
+
+    protected Class getType() {
+        return Integer.class;
+    }
+
+    protected Class getComponentType() {
+        return ToggleBar.class;
+    }
+
+    protected void updateUI(Integer newValue) {
+
+        getComponent().setSelectedIndex(newValue);
+
+    }
+
+    public void valueChanged(ListSelectionEvent e) {
+        callWhenUIChanged(getComponent().getSelectedIndex());
+    }
+
+    protected boolean isSelectionBind() {
+        return true;
+    }
+
+
+}

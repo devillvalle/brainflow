535c7e95fe079e6245be9953a391d527d35c8dbc
brad.buchsbaum@gmail.com <brad.buchsbaum@gmail.com@922ecd08-ad19-0410-8a94-01093a00bd73>
2008-11-03 01:14:46 +0000

    Added number labels to BiSlider

--- trunk/src/com/brainflow/core/layer/ImageLayer3D.java
+++ trunk/src/com/brainflow/core/layer/ImageLayer3D.java
@@ -7,8 +7,7 @@ import com.brainflow.core.layer.ImageLayerProperties;
 import com.brainflow.core.SliceRenderer;
 import com.brainflow.image.anatomy.AnatomicalPoint3D;
 import com.brainflow.image.anatomy.Anatomy3D;
-import com.brainflow.image.data.IImageData;
-import com.brainflow.image.data.IImageData3D;
+import com.brainflow.image.data.*;
 import com.brainflow.image.space.IImageSpace;
 import com.brainflow.image.space.ImageSpace3D;
 import com.brainflow.image.space.IImageSpace3D;
@@ -17,6 +16,10 @@ import com.brainflow.image.interpolation.NearestNeighborInterpolator;
 
 import java.util.Map;
 import java.util.HashMap;
+
+import net.java.dev.properties.Property;
+import net.java.dev.properties.container.ObservableProperty;
+import net.java.dev.properties.container.BeanContainer;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,48 +30,67 @@ import java.util.HashMap;
  */
 
 
+public class ImageLayer3D extends ImageLayer<IImageSpace3D> {
 
 
-public class ImageLayer3D extends ImageLayer<ImageSpace3D>  {
+    public final Property<MaskProperty3D> maskProperty = ObservableProperty.create();
 
 
     public ImageLayer3D(ImageLayer3D layer) {
-        super(layer.getDataSource(), new ImageLayerProperties(layer.getImageLayerProperties()));           
+        super(layer.getDataSource(), new ImageLayerProperties(layer.getImageLayerProperties()));
+        init();
     }
 
     public ImageLayer3D(IImageData data) {
         super(new MemoryImageDataSource(data));
+        init();
     }
 
     public ImageLayer3D(IImageData data, ImageLayerProperties _params) {
         super(new MemoryImageDataSource(data), _params);
+        init();
     }
 
 
     public ImageLayer3D(IImageDataSource dataSource) {
         super(dataSource);
+        init();
     }
 
     public ImageLayer3D(IImageDataSource dataSource, ImageLayerProperties _params) {
         super(dataSource, _params);
+        init();
     }
 
+    private void init() {
+        BeanContainer.bind(this);
+        maskProperty.set(new MaskProperty3D(this));
+
+    }
+
+
+
     public IImageData3D getData() {
-        return  (IImageData3D)super.getData();    //To change body of overridden methods use File | Settings | File Templates.
+        return (IImageData3D) super.getData();
     }
 
     public double getValue(AnatomicalPoint3D pt) {
-       IImageSpace space = getCoordinateSpace();
+        IImageSpace space = getCoordinateSpace();
 
-        AnatomicalPoint3D apt = pt.convertTo((ICoordinateSpace3D)space);
+        AnatomicalPoint3D apt = pt.convertTo((ICoordinateSpace3D) space);
 
-        return getData().worldValue((float)apt.getX(), (float)apt.getY(), (float)apt.getZ(), new NearestNeighborInterpolator());
+        return getData().worldValue((float) apt.getX(), (float) apt.getY(), (float) apt.getZ(), new NearestNeighborInterpolator());
 
-        
+
     }
 
+    public MaskProperty3D getMaskProperty() {
+        return maskProperty.get();
+    }
 
-
+    public void setMaskProperty(MaskProperty3D mask) {
+        maskProperty.set(mask);
+    }
 
     private Map<Anatomy3D, BasicImageSliceRenderer> rendererMap = new HashMap<Anatomy3D, BasicImageSliceRenderer>();
 
@@ -80,7 +102,7 @@ public class ImageLayer3D extends ImageLayer<ImageSpace3D>  {
             rendererMap.put(displayAnatomy, renderer);
 
         } else {
-            renderer =  new BasicImageSliceRenderer(refspace, this, slice, displayAnatomy);
+            renderer = new BasicImageSliceRenderer(refspace, this, slice, displayAnatomy);
             rendererMap.put(displayAnatomy, renderer);
 
         }
@@ -89,7 +111,7 @@ public class ImageLayer3D extends ImageLayer<ImageSpace3D>  {
     }
 
     public SliceRenderer getSliceRenderer(IImageSpace refspace, AnatomicalPoint3D slice, Anatomy3D displayAnatomy) {
-        return getSliceRenderer((IImageSpace3D)refspace, slice, displayAnatomy);
+        return getSliceRenderer((IImageSpace3D) refspace, slice, displayAnatomy);
     }
 
 

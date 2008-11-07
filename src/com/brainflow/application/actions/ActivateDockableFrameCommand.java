60777b73d7e7dffa58a0d1eca3a049d3f8e90bea
brad.buchsbaum <brad.buchsbaum@gmail.com>
2008-11-07 03:27:12 +0000

    Added number labels to BiSlider

--- src/com/brainflow/application/actions/ActivateDockableFrameCommand.java
+++ src/com/brainflow/application/actions/ActivateDockableFrameCommand.java
@@ -1,6 +1,8 @@
 package com.brainflow.application.actions;
 
 import com.jidesoft.docking.DockableFrame;
+import com.jidesoft.docking.DockingManager;
+import com.pietschy.command.toggle.ToggleVetoException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -9,22 +11,35 @@ import com.jidesoft.docking.DockableFrame;
  * Time: 6:21:27 PM
  * To change this template use File | Settings | File Templates.
  */
-public class ActivateDockableFrameCommand extends BrainFlowCommand {
+public class ActivateDockableFrameCommand extends BrainFlowToggleCommand {
 
     DockableFrame dframe;
 
     public ActivateDockableFrameCommand(DockableFrame _dframe) {
         //super("activate-dock");
         dframe = _dframe;
-        //getDefaultFace().setIcon(dframe.getIcon());
+        if (!dframe.isHidden()) {
+            setSelected(true);
+        } else {
+            setSelected(false);
+        }
 
 
     }
 
-    protected void handleExecute() {
+    protected void handleSelection(boolean b) throws ToggleVetoException {
+        if (b) {
+            if (dframe.isHidden() || dframe.isDocked()) {
+                DockingManager dman = dframe.getDockingManager();
+                if (dman != null)
+                    dman.showFrame(dframe.getTitle());
+            }
+        } else {
+            DockingManager dman = dframe.getDockingManager();
+            dman.hideFrame(dframe.getName());
 
-         if (dframe.isHidden() || dframe.isDocked() || !dframe.isActive()) {
-            dframe.getDockingManager().showFrame(dframe.getTitle());
         }
     }
+
+    
 }

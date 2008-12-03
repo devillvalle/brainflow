af6b6ac1ad787d8eac9899472bba664df47c5731
brad.buchsbaum <brad.buchsbaum@gmail.com>
2008-12-03 15:59:01 +0000

    Added number labels to BiSlider

---
+++ src/com/brainflow/application/toplevel/TestScala.scala
@@ -0,0 +1,15 @@
+package com.brainflow.application.toplevel
+/**
+ * Created by IntelliJ IDEA.
+ * User: Brad
+ * Date: Nov 28, 2008
+ * Time: 10:17:52 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+object TestScala {
+
+  def main(args:Array[String]) {
+    Console.println("hello world")
+  }
+}
